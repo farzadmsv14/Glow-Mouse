fe1373c007c62c4c5062dbf717f4c3afb85c10cc
farzadmsv14 <farzadmsv14@gmail.com>
2025-04-23 07:46:56 +0330

    new-v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, HostListener, OnInit } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'testMous';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    const glowCursor = document.getElementById('glowCursor');
+    let mouseX = 0;
+    let mouseY = 0;
+
+    document.addEventListener('mousemove', (event) => {
+      mouseX = event.clientX;
+      mouseY = event.clientY;
+    });
+
+    function animate() {
+      if (glowCursor) {
+        glowCursor.style.transform = `translate(${mouseX}px, ${mouseY}px) translate(-50%, -50%)`;
+      }
+      requestAnimationFrame(animate);
+    }
+
+    animate();
+  }
 }
